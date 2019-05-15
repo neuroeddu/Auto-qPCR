33fc7eaa5444b804dd118e9c51810d5529f79969
Rhalena Thomas <37192426+RhalenaThomas@users.noreply.github.com>
2019-05-15 12:27:02 -0400

    add Iva's automated qPCR AQ

---
+++ config.py
@@ -0,0 +1,79 @@
+from configparser import ConfigParser, NoSectionError, NoOptionError
+from pathlib import Path
+import helpers as h
+
+CONFIG = {}
+
+# Default configuration
+def set_default_config():
+    global CONFIG
+    CONFIG = {
+        'PROJECT' : {
+            # Root project directory - defaults to the system's current working directory
+            'Directory': Path.cwd(),
+            # Look for nested project directories
+            'Recurse': False,
+            # Clean and re-process project directorie with output files
+            'ReRun': False,
+            # Input file extensions
+            'FileExt': "CSV",
+            # Output file tag
+            'DoneTag': "DONE"
+        },
+        'FILE': {
+            # List of tags to extract from the files. Format is "file_line:tag_name1, file_line:tag_name2, ...
+            'Tags': "",
+            # CSV header file line number
+            'HeaderRow': 0,
+            # Commaseparated list of colum indexes to gather
+            'Columns': "",
+            # File encoding
+            'Encoding': "utf-8"
+        },
+        'MODEL': {
+            # The names of the control genes
+            'ControlGenes': "",
+            # The order in which samples will appear in data tables and graphs
+            'SampleOrder': "",
+            # Threshold value when looking for outliers
+            'OutlierCutoff': 0.0,
+            # What percentage of the data set can be purged as outliers before giving up
+            'MaxOutliers': 0.0
+            #
+        }
+    }
+
+# Update the configuration from a global configuration file
+def set_global_config(sname):
+    # Search for a global configuration file in the directory of the script
+    sf = Path.cwd().joinpath(sname)
+    cf = Path(sf).parent.joinpath(Path(sf).stem + ".conf")
+    if not cf.is_file():
+        h.log(h.LOG_VERBOSE, "No global configuration file found")
+        return
+    h.log(h.LOG_VERBOSE, "Global configuration file: {}".format(cf))
+    set_config(CONFIG, cf)
+
+# Update a configuration from a configuration file
+def set_config(cfg, cfg_file):
+    p = ConfigParser(
+        strict = True,
+        comment_prefixes = ('#'),
+        allow_no_value = False,
+        inline_comment_prefixes = None,
+        delimiters = ('='),
+        empty_lines_in_values = False)
+    # Next override is to preserve the case of the options (the default is to convert them to lowercase)
+    p.optionxform = lambda option: option
+    p.read(cfg_file)
+    # Update the configuration from the file, preserving the option value types
+    for section in cfg:
+        for option, value in cfg[section].items():
+            try:
+                if type(value) == bool:
+                    cfg[section][option] = p.getboolean(section, option)
+                else:
+                    cfg[section][option] = (type(value))(p.get(section, option))
+            # Ignore missing sections and options
+            except (NoSectionError, NoOptionError):
+                pass
---
+++ idlab.py
@@ -0,0 +1,52 @@
+# ID Lab qPCR Analysis
+# Zero Clause BSD Copyright (c) 2019 by Iveta Demirova
+
+VERSION = "0.1.5"
+QUALITY = ""
+
+from sys import argv
+from pathlib import Path
+from argparse import ArgumentParser
+import helpers as h
+import config as c
+import project as p
+
+def main():
+    h.verbosity = h.LOG_DEBUG
+    c.set_default_config()
+    if __debug__:
+        if QUALITY == "DEV":
+            h.verbosity = h.LOG_DEBUG
+            c.CONFIG['PROJECT']['Directory'] = Path.home().joinpath \
+            ("Documents/McGill/_Research/Durcan_Lab/qPCR_program/qPCR_Program-0.1.5/idlab/Gilles_plates")
+    c.set_global_config(argv[0])
+
+    get_cmd_args()
+
+    p.process_root_directory()
+    h.log(h.LOG_INFO, "\nAll done")
+
+# Update the configuration from the command line arguments
+def get_cmd_args():
+    p = ArgumentParser(description = "ID Lab qPCR Analysis v" + VERSION + " " + QUALITY)
+    p.add_argument(
+        '-d', '--directory', nargs='?', help = "project directory", dest='Directory')
+    p.add_argument(
+        "-r", '--recurse', dest = "Recurse", action='store_true', help = "recurse the project directory")
+    p.add_argument(
+        "-v", '--verbosity', dest = "Verbosity", type = int, choices = range(0,10),
+        help="verbosity [{}:Quiet, {}+:Info, {}+:Verbose, {}:Debug]".format(
+            h.LOG_QUIET, h.LOG_INFO, h.LOG_VERBOSE, h.LOG_DEBUG))
+    args=vars(p.parse_args())
+
+    if args["Directory"] is not None:
+        c.CONFIG['PROJECT']['Directory'] = args["Directory"]
+
+    if args["Recurse"] and not c.CONFIG['PROJECT']['Recurse']:
+        c.CONFIG['PROJECT']['Recurse'] = True
+
+    if args["Verbosity"] is not None:
+        h.verbosity = args["Verbosity"]
+
+if __name__ == "__main__":
+    main()
