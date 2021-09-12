6905ddc146ad800635c1f085d2de6e3585762f3d
rhalena <rhalena.t@gmail.com>
2021-09-12 15:56:56 -0400

    test local and test updating packages

---
+++ website/Files_to_link_website/Local_download/Auto-q-pcr-frontend/application/relative.py
@@ -0,0 +1,125 @@
+# Auto-q-PCR is a program for analysis of qPCR data for absolute and relative quantification
+# Copyright (C) 2021 Rhalena Thomas, Eddie Cai, Gracia Gu and Iva Demirova
+#This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation,  version 3 of the License.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+
+#########################################################################################################
+
+# run the model relative delta-CT to calculate the normalized CT values
+
+import pandas
+import numpy as np
+import re
+
+
+def process(data, colnames, target_sorter, sample_sorter):
+
+	outlier_data = data[data['Outliers'].eq(True)]
+	data = data[data['Outliers'].eq(False)]
+
+	# Calculate CT Mean (Endogenous Control Mean) and SSD for all Controls
+
+	control_filter = (data['Control'].eq(True))
+	data_controls_grouped = data[control_filter].groupby(['Target Name' , 'Sample Name', 'filename'], sort=False).agg(
+		{'CT': [np.size , 'mean' , 'std']})
+
+	# print("Endogenous Control CT Means and SSD")
+	# print(data_controls_grouped)
+
+	data_controls_ct = data[control_filter].groupby(['Sample Name', 'filename'], sort=False).agg({'CT': 'mean'})
+
+	# print("Combined Endogenous Control CT Means and SSD")
+	#print(data_controls_ct)
+	#print(data['filename'])
+	# Create rq column
+	for i , row in enumerate(data_controls_ct.itertuples(name=None) , 1):
+		print(row[0])
+		name_filter = (data['Sample Name'] == row[0][0])
+		file_filter = (data['filename'] == row[0][1])
+		for j in data[name_filter][file_filter].index:
+			data.loc[j , 'rq'] = np.power(2 , -(data.loc[j , 'CT'] - row[1]))
+
+	# Calculate the SEM for technical replicate groups
+	targets = data['Target Name'].drop_duplicates(keep='first').values
+	mean_sem_result = {}
+	for target in targets:
+		mean_sem_result[target] = {}
+		samples = data[data['Target Name'] == target]['Sample Name'].drop_duplicates(keep='first').values
+		for sample in samples:
+			target_sample_data = data[(data['Target Name'] == target) & (data['Sample Name'] == sample)]
+			mean = target_sample_data['rq'].mean()
+			sdt_dev = target_sample_data['rq'].std()
+			std_err = target_sample_data['rq'].sem()
+			mean_sem_result[target][sample] = (mean , sdt_dev , std_err)
+	for i_row , row in data.iterrows():
+		if data.at[i_row , 'Sample Name'] in samples and data.at[i_row , 'Sample Name'] in samples and data.at[
+			i_row , 'Target Name'] in mean_sem_result and data.at[i_row , 'Sample Name'] in mean_sem_result[
+			data.at[i_row, 'Target Name']]:
+			data.at[i_row , 'rqMean'] = mean_sem_result[data.at[i_row , 'Target Name']][data.at[i_row , 'Sample Name']][
+				0]
+			data.at[i_row, 'rqSD'] = mean_sem_result[data.at[i_row, 'Target Name']][data.at[i_row , 'Sample Name']][1]
+			data.at[i_row, 'rqSEM'] = mean_sem_result[data.at[i_row, 'Target Name']][data.at[i_row, 'Sample Name']][2]
+
+	# Making the intermediate dataframe
+	data = data.append(outlier_data)  # add outliers
+	# Sorting data
+	data = data_sorter(data, target_sorter, sample_sorter)
+
+	cnames = [c.strip().lower() for c in colnames.split(',')]
+	clist = []
+	for c in data.columns.values.tolist():
+		if c.lower() in cnames:
+			clist.append(c)
+	cols = ['Target Name', 'Sample Name', 'filename', 'rq', 'rqMean', 'rqSD', 'rqSEM', 'Outliers'] + clist
+	df = pandas.DataFrame(columns=cols)
+	for item in cols:
+		df[item] = data[item]
+	df.reset_index(drop=True , inplace=True)
+
+	# remove outliers in summary data
+	data = data[data['Outliers'].eq(False)]
+	data_output_summary = data.groupby(['Target Name', 'Sample Name', 'filename'], sort=False).agg(
+		{'rq': [np.size , 'mean'] , 'rqSD': 'mean', 'rqSEM': 'mean'})
+
+	data_output_summary_w_group = data.groupby(['Target Name', 'Sample Name', 'filename']+clist, sort=False).agg(
+		{'rq': [np.size , 'mean'] , 'rqSD': 'mean', 'rqSEM': 'mean'})
+
+	targets = data['Target Name'].drop_duplicates(keep='first').values
+	samples = data['Sample Name'].drop_duplicates(keep='first').values
+
+	return df, data_output_summary, data_output_summary_w_group, targets, samples
+
+
+def data_sorter(data, target_sorter, sample_sorter):
+	# define sorter for target name order based on list
+	targets = data['Target Name'].drop_duplicates(keep='first').values
+	if target_sorter != '':
+		targets = [sorter.strip() for sorter in target_sorter.split(',')]
+	# remove nan from list
+	targets = [t for t in targets if type(t) is not float]
+	# Add sorter to dataframe to order Target Names in output files
+	sorter_index = dict(zip([g.lower() for g in targets], range(len(targets))))
+	data['Target Order'] = data['Target Name'].str.lower().map(sorter_index)
+	data.sort_values(['Target Order'], inplace=True)
+
+	# define sorter for sample name order based on list
+	sorter = data['Sample Name'].drop_duplicates(keep='first').values
+	if sample_sorter != '':
+		sorter = [sorter.strip() for sorter in sample_sorter.split(',')]
+	# remove nan from list
+	sorter = [s for s in sorter if type(s) is not float]
+	# Add sorter to dataframe to order Sample Names in output files
+	sorter_index = dict(zip([s.lower() for s in sorter], range(len(sorter))))
+	data['Sample Name Key'] = data['Sample Name'].str.extract(
+		re.compile('(' + '|'.join(sorter) + ')', re.IGNORECASE), expand=False).fillna('')
+	data['Sample Order'] = data['Sample Name Key'].str.lower().map(sorter_index)
+	data.sort_values(by=['Target Order', 'Sample Order'], inplace=True)
+
+	return data
---
+++ website/Files_to_link_website/Local_download/Auto-q-pcr-frontend/application/static/js/scripts.js
@@ -0,0 +1,125 @@
+// Add the following code if you want the name of the file appear on select
+document.querySelector(".custom-file-input").addEventListener("change",function(e){
+	$(this).next(".custom-file-label").html(e.target.files.length + " file(s) uploaded");
+});
+
+// Display and hide file information fields
+document.getElementById("info_form").style.display = "none";
+
+document.getElementById("y_fileinfo").onclick = function() {
+	document.getElementById("info_form").style.display = "block";
+};
+
+document.getElementById("n_fileinfo").onclick = function() {
+	document.getElementById("genes").value = "";
+	document.getElementById("quencher").value = "";
+	document.getElementById("task").value = "UNKNOWN";
+	document.getElementById("info_form").style.display = "none";
+};
+
+// CSample is only enabled and required when relative ddCT or instability is selected
+document.getElementById("csample").setAttribute("disabled", "");
+
+document.getElementById("specialStar").style.display = "none";
+
+document.getElementById("absolute").onclick = function() {
+	document.getElementById("csample").setAttribute("disabled", "");
+	document.getElementById("specialStar").style.display = "none";
+};
+
+document.getElementById("relative_dCT").onclick = function() {
+	document.getElementById("csample").setAttribute("disabled", "");
+	document.getElementById("specialStar").style.display = "none";
+};
+
+document.getElementById("relative_ddCT").onclick = function() {
+	document.getElementById("csample").setAttribute("required","");
+	document.getElementById("csample").removeAttribute("disabled");
+	document.getElementById("specialStar").style.display = "inline-block";
+};
+
+document.getElementById("instability").onclick = function() {
+	document.getElementById("csample").setAttribute("required","");
+	document.getElementById("csample").removeAttribute("disabled");
+	document.getElementById("specialStar").style.display = "inline-block";
+};
+
+// display or hide stats form
+document.getElementById("stats_form").style.display = "none";
+
+document.getElementById("y_stats").onclick = function() {
+	document.getElementById("stats_form").style.display = "block";
+};
+
+document.getElementById("n_stats").onclick = function() {
+	document.getElementById("colnames").value = "";
+	document.getElementById("quantity").value = "";
+	document.getElementById("opt-gcol").checked = true;
+	document.getElementById("opt-glist").checked = false;
+	document.getElementById("glist").value = "";
+	document.getElementById("glist1").value = "";
+	document.getElementById("glist2").value = "";
+	document.getElementById("gcol").value = "";
+	document.getElementById("gcol1").value = "";
+	document.getElementById("gcol2").value = "";
+	document.getElementById("colname1").value = "";
+	document.getElementById("colname2").value = "";
+	document.getElementById("y_rm").checked = false;
+	document.getElementById("n_rm").checked = true;
+	document.getElementById("y_nd").checked = true;
+	document.getElementById("n_nd").checked = false;
+	document.getElementById("stats_form").style.display = "none";
+};
+
+// disable group column or group list when the other option is selected
+// display or hide one-way or two-way ANOVA
+document.getElementById("opt-gcol").setAttribute("checked","");
+document.getElementById("oneway").setAttribute("checked", "");
+document.getElementById("col-form").style.display = "inline";
+document.getElementById("list-form").style.display = "none";
+document.getElementById("group-col2").style.display = "none";
+document.getElementById("group-list2").style.display = "none";
+
+document.getElementById("opt-gcol").onclick = function() {
+	document.getElementById("col-form").style.display = "block";
+	document.getElementById("list-form").style.display = "none";
+	document.getElementById("glist").value = "";
+	document.getElementById("colname1").value = "";
+	document.getElementById("colname2").value = "";
+	document.getElementById("glist1").value = "";
+	document.getElementById("glist2").value = "";
+};
+
+document.getElementById("opt-glist").onclick = function() {
+	document.getElementById("col-form").style.display = "none";
+	document.getElementById("list-form").style.display = "block";
+	document.getElementById("gcol").value = "";
+	document.getElementById("gcol1").value = "";
+	document.getElementById("gcol2").value = "";
+};
+
+document.getElementById("oneway").onclick = function() {
+	document.getElementById("group-col2").style.display = "none";
+	document.getElementById("group-list2").style.display = "none";
+	document.getElementById("group-col").style.display = "block";
+	document.getElementById("group-list").style.display = "block";
+	document.getElementById("colname1").value = "";
+	document.getElementById("colname2").value = "";
+	document.getElementById("glist1").value = "";
+	document.getElementById("glist2").value = "";
+}
+
+document.getElementById("twoway").onclick = function() {
+	document.getElementById("group-col").style.display = "none";
+	document.getElementById("group-list").style.display = "none";
+	document.getElementById("group-col2").style.display = "block";
+	document.getElementById("group-list2").style.display = "block";
+	document.getElementById("gcol").value = "";
+	document.getElementById("gcol1").value = "";
+	document.getElementById("gcol2").value = "";
+}
+
+// clear form
+document.getElementById("clear").onclick = function() {
+	location.reload();
+};
