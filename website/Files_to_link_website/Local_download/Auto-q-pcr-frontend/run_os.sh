58fb79b1a4093b515d33fcf74f3f65f7e28a259b
rhalena <rhalena.t@gmail.com>
2021-09-13 09:05:50 -0400

    adjusted stats interface messages and working on requirments file

--- website/Files_to_link_website/Local_download/Auto-q-pcr-frontend/run_os.sh
+++ website/Files_to_link_website/Local_download/Auto-q-pcr-frontend/run_os.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python3 -m virtualenv venv
-source venv venv/bin/activate 
+python3 -m venv local-env
+source loca-env/bin/activate 
 python3 -m pip install -r requirements.txt
 python3 -m webbrowser http://127.0.0.1:5000/
 python3 main.py
