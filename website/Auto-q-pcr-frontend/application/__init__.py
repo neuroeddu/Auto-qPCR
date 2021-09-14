f4c0483ad0503ba53a69b72d8690dcb766501077
rhalena <rhalena.t@gmail.com>
2021-09-13 21:51:56 -0400

    update email again

--- website/Auto-q-pcr-frontend/application/__init__.py
+++ website/Auto-q-pcr-frontend/application/__init__.py
@@ -21,12 +21,12 @@ mail = Mail()
 app = Flask(__name__)
 
 app.static_folder = 'static'
-app.secret_key = b'6Le3c7oZAAAAADF6jktQ2xuxnb1I1tlODKQwaWxU'
+app.secret_key = b'6Leez0scAAAAABp0A4Fi85tPDhz70dgRVXcX4g3f'
 
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_SERVER"] = 'Exchange'
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = 'autoqpcr@gmail.com'
-app.config["MAIL_PASSWORD"] = 'neuroeddu'
+app.config["MAIL_USERNAME"] = 'neuroeddu.mni@mcgill.ca'
+app.config["MAIL_PASSWORD"] = ''
 app.config['MAIL_USE_SSL'] = True
 
 app.config['UPLOAD_FOLDER'] = 'static/files/'
