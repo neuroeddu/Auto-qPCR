41b1c26e5c6eaf9e6f27df27d94ab30ae2691c30
rhalena <rhalena.t@gmail.com>
2021-11-26 14:31:03 -0500

    changed requirments.txt and tried to update front page

--- website/Files_to_link_website/sample data/Local_Download/Auto-q-pcr-frontend/run_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 -m venv local-env
-source loca-env/bin/activate 
-python3 -m pip install -r requirements.txt
-python3 main.py
-#$SHELL
--- website/Files_to_link_website/sample data/Local_Download/Auto-q-pcr-frontend/run_os.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m venv local-env
-source loca-env/bin/activate 
-python3 -m pip install -r requirements.txt
-python -m webbrowser http://127.0.0.1:5000/
-python3 main.py
-#$SHELL
